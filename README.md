d6c2a925de9558f33201cf2be59444d37a9aa9cf
giulianna <j.bustinza42@gmail.com>
2016-12-27 16:12:12 -0500

    fin

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # ejercicio9parrot
-# Inicio
-- horas
+
+# Inicio: Evaluar si estamos en problemas ?
+
+# Ingreso datos
+
+- hora
 - talking
+- estamos_en problemas
 
 # Proceso
 
--  if((hora<7 || hora>20) && talking==1){
-        return 1;
-    }
-    else{
-        return 0;
-    }
+- Obtener valor hora
+- Obtener valor talking
+- Si 7>hora>20 estamos en problemas
+- Si talking es verdadero 
+    - Mostrar 1  
+- Si talking no es verdadero 
+    -Mostrar 0 
 
-}
+
+
+
+# Fin 
+
--- main.js
+++ main.js
@@ -1,44 +1,18 @@
+
+
 function calcular()
 {
-	var talking = document.getElementById("hablando").value;
-	var hora = parseInt(document.getElementById("hora").value);
-
-	var istalking = true;
-	var isnottalking = false;
-
-	var salida = document.getElementById("salida");
-
-	if(talking == "2")
-	{
-		isVacation = false;
-	}
-
-	if(weekday < 6)
-	{
-		isWeekday = true;
-	}
-
-	if(isVacation)
-	{
-		salida.innerHTML = "1";
-	}else{
-		if(isWeekday)
-		{
-			salida.innerHTML = "0";
-		}else{
-			salida.innerHTML = "1";
-		}
-	}
-}
-
-
-
- talking = Boolean(talking);
+    var hora = document.getElementById("hora").value;
+	var talking = document.getElementById("talking").value;
+    var salida = document.getElementById("salida");
+    
     if((hora<7 || hora>20) && talking==1){
-        return 1;
+        salida.innerHTML=" hay problemas  1";
     }
-    else{
-        return 0;
+    else {
+        salida.innerHTML="no hay problemas  0";
     }
 
 }
+
+
